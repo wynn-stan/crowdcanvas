fb5941b340ff188355d7c0dd0c130f557f665700
inpath-winston <117194280+inpath-winston@users.noreply.github.com>
2023-12-22 12:47:40 +0000

    fix back on post

--- app/components/ToHome/ToHome.tsx
+++ app/components/ToHome/ToHome.tsx
@@ -1,13 +1,21 @@
 "use client";
 
 import { X } from "lucide-react";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 export default function ToHome({ children }: { children?: React.ReactNode }) {
+  //variables
+  const base = usePathname().split("/")[1];
+  const path = (() => {
+    if (base === "events") return "/events/explore";
+    return "/home";
+  })();
+
+  //navigation
   const router = useRouter();
 
   return (
-    <div onClick={() => router.back()} className="cursor-pointer">
+    <div onClick={() => router.push(path)} className="cursor-pointer">
       {children ? <>{children}</> : <X />}
     </div>
   );
